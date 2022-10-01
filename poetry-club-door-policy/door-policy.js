ec6468a532e086f84871c12924749791e41ec88f
@nullthefirst <ninte.dev@gmail.com>
2022-10-01 20:11:10 +0100

    Completed 'back door password' function

--- poetry-club-door-policy/door-policy.js
+++ poetry-club-door-policy/door-policy.js
@@ -55,5 +55,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-    throw new Error("Remove this line and implement the function");
+    return `${capitalizer(word)}, please`;
 }
