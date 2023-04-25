9cd93345a1d8b5eaea5b41027e7e25c144cfc684
Tey Dangana <tey@nullnow.com>
2023-04-25 15:56:15 +0100

    Completed poetry club door policy exercise

--- poetry-club-door-policy/door-policy.js
+++ poetry-club-door-policy/door-policy.js
@@ -27,7 +27,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line[0];
 }
 
 /**
@@ -38,7 +38,11 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  word = word.toLowerCase();
+  let wordBucket = [word[0], word.slice(1)];
+  const firstLetter = wordBucket[0].toUpperCase();
+  const password = firstLetter + wordBucket[1];
+  return password;
 }
 
 /**
@@ -49,7 +53,8 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  line = line.trim();
+  return line[line.length - 1];
 }
 
 /**
@@ -60,5 +65,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  return `${frontDoorPassword(word)}, please`;
 }
