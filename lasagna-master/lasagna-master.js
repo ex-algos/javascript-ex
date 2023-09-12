6c3fe023a2f24b8bd44f14507ca79bb5f6908d82
nullthefirst <tey@nullnow.com>
2023-09-12 16:00:58 +0100

    Completed cookingStatus function

--- lasagna-master/lasagna-master.js
+++ lasagna-master/lasagna-master.js
@@ -1,12 +1,20 @@
 /// <reference path="./global.d.ts" />
 // @ts-check
 
-/**
- * Implement the functions needed to solve the exercise here.
- * Do not forget to export them so they are available for the
- * tests. Here an example of the syntax as reminder:
- *
- * export function yourFunction(...) {
- *   ...
- * }
- */
+export function cookingStatus(remainingTime) {
+  const invalid = null || typeof remainingTime !== 'number';
+
+  if (remainingTime === undefined) {
+    return 'You forgot to set the timer.';
+  } else {
+    if (remainingTime === invalid) {
+      return 'Timer input is invalid.';
+    } else {
+      if (remainingTime === 0) {
+        return 'Lasagna is done.';
+      } else {
+        return 'Not done, please wait.';
+      }
+    }
+  }
+}
