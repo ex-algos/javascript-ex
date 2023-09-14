a28a4911ab6269c476a7befce3024f215ca1d123
nullthefirst <tey@nullnow.com>
2023-09-14 11:29:54 +0100

    Completed scale2d function with closures

--- coordinate-transformation/coordinate-transformation.js
+++ coordinate-transformation/coordinate-transformation.js
@@ -34,7 +34,12 @@ export function translate2d(dx, dy) {
  *  scaled coordinate pair in the form [x, y]
  */
 export function scale2d(sx, sy) {
-  throw new Error('Implement the scale2d function');
+  return function output(x, y) {
+    const newX = x * sx;
+    const newY = y * sy;
+
+    return [newX, newY];
+  };
 }
 
 /**
