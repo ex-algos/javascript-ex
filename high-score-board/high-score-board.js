47f263cd620656c6ee4dca38e83cd5680f4244f4
Usheninte Dangana <tey@nullnow.com>
2024-07-26 15:11:12 +0100

    Completed updateScore exercise.

--- high-score-board/high-score-board.js
+++ high-score-board/high-score-board.js
@@ -50,7 +50,9 @@ export function removePlayer(scoreBoard, player) {
  * @returns {Record<string, number>} updated score board
  */
 export function updateScore(scoreBoard, player, points) {
-  throw new Error('Please implement the updateScore function');
+  scoreBoard[player] += points;
+
+  return scoreBoard;
 }
 
 /**
