3135e4b8a1ea0034b3301ad914b7f78c2c2f344c
Usheninte Dangana <tey@nullnow.com>
2024-08-05 10:03:09 +0100

    Completed gtc version task.

--- amusement-park/amusement-park.js
+++ amusement-park/amusement-park.js
@@ -79,5 +79,5 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @returns {string | undefined} version
  */
 export function gtcVersion(visitor) {
-  throw new Error('Please implement the gtcVersion function.');
+  return visitor.gtc?.version;
 }
